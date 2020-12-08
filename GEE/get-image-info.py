aa15205c1f8146e786c5d0a7494d075ca2f66259
chenxw <632418175@qq.com>
2020-12-09 01:38:28 +0800

    调通本地调用GEE

--- GEE/get-image-info.py
+++ GEE/get-image-info.py
@@ -16,18 +16,4 @@ os.environ['HTTP_PROXY'] = 'http://127.0.0.1:1080'
 os.environ['HTTPS_PROXY'] = 'http://127.0.0.1:1080'
 
 ee.Initialize()
-image1 = ee.Image('srtm90_v4')
-path = image1.getDownloadUrl({
-    'scale': 30,
-    'crs': 'EPSG:4326',
-    'region': '[[-120, 35], [-119, 35], [-119, 34], [-120, 34]]'
-})
-# 获取下载地址
-print(path)
-thumb_url=image1.getThumbURL({
-    "crs":"EPSG:4326",
-    "min":0,
-    "max":5000
-})
-print(thumb_url)
-Image(url=thumb_url)
+print(ee.Image('USGS/SRTMGL1_003').getInfo())
